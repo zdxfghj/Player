4a49261c6c2c4e1a3443a070a93a48afc880ecdd
Vladislau <test@yandex.by>
2022-12-16 16:39:41 +0300

    creat player

---
+++ script.js
@@ -0,0 +1,7 @@
+let audio = document.getElementById("audio");    // Берём элемент audio
+let time = document.querySelector(".time");      // Берём аудио дорожку
+let btnPlay = document.querySelector(".play");   // Берём кнопку проигрывания
+let btnPause = document.querySelector(".pause"); // Берём кнопку паузы
+let btnPrev = document.querySelector(".prev");   // Берём кнопку переключения предыдущего трека
+let btnNext = document.querySelector(".next"); 
+
